51e2d99735d28a6e7c16a554d11276dc3e298427
professional Developper <225871770+progammer24052025-png@users.noreply.github.com>
2026-06-22 09:03:12 +0530

    ready for render deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      'localhost',
+      '[IP_ADDRESS]',
+      'postmeningeal-homeoplastic-janiyah.ngrok-free.dev'
+    ],
+  },
 })
